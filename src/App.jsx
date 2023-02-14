0197e9a36f00a329a00ea62968305f9066399fb8
Bryan Armando <mamanibryan1@gmail.com>
2023-02-14 12:50:40 -0500

    Fix Background image in App

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import { useContext } from "react";
 function App() {
     const { handleDrangAndDrop } = useContext(useTodoContext);
     return (
-        <div className="mx-auto min-h-screen bg-gray-100 bg-mobile-light bg-contain bg-no-repeat dark:bg-gray-900 dark:bg-mobile-dark md:bg-desk-light md:dark:bg-desk-dark">
+        <div className="mx-auto min-h-screen bg-gray-100 bg-[url('./src/assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat dark:bg-gray-900 dark:bg-[url('./src/assets/images/bg-mobile-dark.jpg')] md:bg-[url('./src/assets/images/bg-desktop-light.jpg')] md:dark:bg-[url('./src/assets/images/bg-desktop-dark.jpg')]">
             <div className="mx-auto max-w-xl">
                 <Header />
                 <main className="container mx-auto px-4">
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,16 +5,11 @@ module.exports = {
     theme: {
         extend: {
             backgroundImage: {
-                "desk-dark": "url('./src/assets/images/bg-desktop-dark.jpg')",
-                "desk-light": "url('./src/assets/images/bg-desktop-light.jpg')",
-                "mobile-dark": "url('./src/assets/images/bg-mobile-dark.jpg')",
-                "mobile-light":
-                    "url('./src/assets/images/bg-mobile-light.jpg')",
                 "check-bg":
                     "linear-gradient hsl(192, 100%, 67%) to hsl(280, 87%, 65%)",
             },
         },
     },
-    darkMode : 'class',
+    darkMode: "class",
     plugins: [],
 };
