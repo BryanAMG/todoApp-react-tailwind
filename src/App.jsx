ed428945a807ff0479a91226905707b20cf50cbc
Bryan Armando <mamanibryan1@gmail.com>
2023-02-14 12:56:18 -0500

    Fix again Background

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import { useContext } from "react";
 function App() {
     const { handleDrangAndDrop } = useContext(useTodoContext);
     return (
-        <div className="mx-auto min-h-screen bg-gray-100 bg-[url('./src/assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat dark:bg-gray-900 dark:bg-[url('./src/assets/images/bg-mobile-dark.jpg')] md:bg-[url('./src/assets/images/bg-desktop-light.jpg')] md:dark:bg-[url('./src/assets/images/bg-desktop-dark.jpg')]">
+        <div className="mx-auto min-h-screen bg-gray-100 bg-[url('./assets/images/bg-mobile-light.jpg')] bg-contain bg-no-repeat dark:bg-gray-900 dark:bg-[url('./assets/images/bg-mobile-dark.jpg')] md:bg-[url('./assets/images/bg-desktop-light.jpg')] md:dark:bg-[url('./assets/images/bg-desktop-dark.jpg')]">
             <div className="mx-auto max-w-xl">
                 <Header />
                 <main className="container mx-auto px-4">
